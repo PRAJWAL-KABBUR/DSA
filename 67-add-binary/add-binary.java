f5f2626ba871bc62ec94e26f6d9d1f2a6c824fbc
Prajwal Kabbur <71619577+PRAJWAL-KABBUR@users.noreply.github.com>
2026-09-01 22:13:03 +0530

    Time: 1 ms (99.95%) | Memory: 43.5 MB (69.41%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -4,7 +4,8 @@ class Solution {
         int i = a.length() - 1;
         int j = b.length() - 1;
         int carry = 0;
-        while (i >= 0 || j >= 0 || carry > 0) {
+        while (i >= 0 || j >= 0 || carry > 0) 
+        {
             int sum = carry;
             if (i >= 0) {
                 sum += a.charAt(i) - '0';
